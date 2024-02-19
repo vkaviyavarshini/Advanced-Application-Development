fd98bd98b1428431ab2ec337e8bae0dcaff5138d
vkaviyavarshini <117342458+vkaviyavarshini@users.noreply.github.com>
2024-02-19 16:28:56 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+import Login from './pages/login'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="App">
+      <Login />
+    </div>
+        
+    </>
+  )
+}
+
+export default App
