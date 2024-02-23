248f394737756494d194c4fc0acdfef6fa77233d
vkaviyavarshini <117342458+vkaviyavarshini@users.noreply.github.com>
2024-02-23 16:34:33 +0530

    Add files via upload

---
+++ frontend/day5/src/pages/productsbycat/hergifts.jsx
@@ -0,0 +1,20 @@
+//import React from 'react';
+import Navigation from '../../pages/navigationBar'; 
+import LeftSidebar from '../../pages/leftsidebar';
+import RightSidebar from '../../pages/rightsidebar';
+//import { Link } from 'react-router-dom';  
+
+
+const GiftsForHer = () => {
+  return (
+    <div>
+      
+      <Navigation/>
+      <LeftSidebar/>  
+      <RightSidebar/>
+
+    </div>
+  );
+};
+
+export default GiftsForHer;
