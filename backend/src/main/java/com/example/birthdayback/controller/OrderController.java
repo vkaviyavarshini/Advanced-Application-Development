8abecdf82e9e2fe9137175dde63c58e11f8d9c3e
vkaviyavarshini <117342458+vkaviyavarshini@users.noreply.github.com>
2024-03-11 14:53:14 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/birthdayback/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.example.birthdayback.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.birthdayback.entity.Order;
+
+import com.example.birthdayback.service.OrderService;
+
+
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+    
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/submit")
+    public ResponseEntity<String> submitOrder(@RequestBody Order order) {
+        orderService.saveOrder(order);
+        return ResponseEntity.ok("Order submitted successfully");
+    }
+}
